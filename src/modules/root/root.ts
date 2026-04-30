bb5aff963463a9127f510cc58bed57db3747b1b3
Fran <187207872+Reyunix@users.noreply.github.com>
2026-04-30 20:09:20 +0200

    health endpoint added

--- src/modules/root/root.ts
+++ src/modules/root/root.ts
@@ -4,4 +4,7 @@ export const rootRoutes = (app: FastifyInstance) => {
   app.get("/", async () => {
     return { message: "Hello from Fastify backend!" };
   });
+  app.get("/healthz", async () => {
+    return { ok: true };
+  });
 };
